160459a1411407b575a6252b021107369ec4abd3
Abdullah Imran <abdullah.imran.code@gmail.com>
2024-04-27 22:39:06 +0500

    fix: syntax

--- src/app/components/Icon.tsx
+++ src/app/components/Icon.tsx
@@ -1,31 +1,73 @@
-import * as React from 'react';
+import * as React from "react";
 
 export const LinkedIn = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin">
-    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/>
-    <rect width="4" height="12" x="2" y="9"/>
-    <circle cx="4" cy="4" r="2"/>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className="lucide lucide-linkedin"
+  >
+    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+    <rect width="4" height="12" x="2" y="9" />
+    <circle cx="4" cy="4" r="2" />
   </svg>
-)
+);
 
 export const Github = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github">
-    <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/>
-    <path d="M9 18c-4.51 2-5-2-7-2"/>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className="lucide lucide-github"
+  >
+    <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+    <path d="M9 18c-4.51 2-5-2-7-2" />
   </svg>
-)
+);
 
 export const Medium = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" enable-background="new 0 0 100 100" viewBox="0 0 100 100" id="medium">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    enable-background="new 0 0 100 100"
+    viewBox="0 0 100 100"
+    id="medium"
+  >
     <g>
-      <path fill="currentColor" d="M29.883 23.814c-14.401 0-26.075 11.751-26.075 26.254 1.375 34.831 50.782 34.815 52.151 0C55.96 35.569 44.284 23.814 29.883 23.814zM71.527 25.354c-7.2 0-13.038 11.068-13.038 24.714.715 32.797 25.363 32.797 26.076 0C84.565 36.422 78.728 25.354 71.527 25.354zM91.681 27.925c-6.023.426-6.01 43.885-.001 44.286C97.701 71.789 97.69 28.324 91.681 27.925z"/>
+      <path
+        fill="currentColor"
+        d="M29.883 23.814c-14.401 0-26.075 11.751-26.075 26.254 1.375 34.831 50.782 34.815 52.151 0C55.96 35.569 44.284 23.814 29.883 23.814zM71.527 25.354c-7.2 0-13.038 11.068-13.038 24.714.715 32.797 25.363 32.797 26.076 0C84.565 36.422 78.728 25.354 71.527 25.354zM91.681 27.925c-6.023.426-6.01 43.885-.001 44.286C97.701 71.789 97.69 28.324 91.681 27.925z"
+      />
     </g>
   </svg>
-)
+);
 
 export const Twitter = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" enable-background="new 0 0 1668.56 1221.19" viewBox="0 0 1668.56 1221.19" id="twitter-x">
-    <path fill="currentColor" d="M283.94,167.31l386.39,516.64L281.5,1104h87.51l340.42-367.76L984.48,1104h297.8L874.15,558.3l361.92-390.99 h-87.51l-313.51,338.7l-253.31-338.7H283.94z M412.63,231.77h136.81l604.13,807.76h-136.81L412.63,231.77z" transform="translate(52.39 -25.059)" />
-  </svg>  
-)
-
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    enable-background="new 0 0 1668.56 1221.19"
+    viewBox="0 0 1668.56 1221.19"
+    id="twitter-x"
+  >
+    <path
+      fill="currentColor"
+      d="M283.94,167.31l386.39,516.64L281.5,1104h87.51l340.42-367.76L984.48,1104h297.8L874.15,558.3l361.92-390.99 h-87.51l-313.51,338.7l-253.31-338.7H283.94z M412.63,231.77h136.81l604.13,807.76h-136.81L412.63,231.77z"
+      transform="translate(52.39 -25.059)"
+    />
+  </svg>
+);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,50 @@
 import * as React from "react";
-import LinkItem from "@/app/components/LinkItem"
-import { LinkedIn, Github, Medium, Twitter } from "@/app/components/Icon"
-import Image from "next/image"
+import LinkItem from "@/app/components/LinkItem";
+import { LinkedIn, Github, Medium, Twitter } from "@/app/components/Icon";
+import Image from "next/image";
 import ProfileImg from "@/app/assets/profile.png";
 import BgImage from "@/app/assets/bg-3.jpg";
 
 const Page = () => {
   return (
-      <div className="h-screen w-full" >
-        <Image src={BgImage} alt='bg-image' className="z-0 w-full h-full object-cover absolute" />
-        <div className='z-10 w-full flex flex-col items-center py-10 px-14'>
-          <Image src={ProfileImg} alt='profile-img' className="border-4 border-white z-10 rounded-full w-40 aspect-square" />
-          <h1 className="mt-6 text-white font-semibold z-10 bg-zinc-900/40 backdrop-blur-md px-4 py-2.5 rounded-full border-2 border-zinc-800" >@Craftzmen</h1>
-          <div className="max-w-md mx-auto flex flex-col gap-5 text-white mt-10 lg:mt-20 w-full">
-            <LinkItem icon={<LinkedIn/>} url={"https://www.linkedin.com/in/abdullah-imran-592550267/"}>Linkedin</LinkItem>
-            <LinkItem icon={<Github/>} url={"https://github.com/Craftzmen/"} >Github</LinkItem>
-            <LinkItem icon={<Medium/>} url={"https://medium.com/@abdullah.imran.code/"} >Medium</LinkItem>
-            <LinkItem icon={<Twitter/>} url={"https://x.com/Craftzmen/"} >Twitter</LinkItem>
-          </div>
+    <div className="h-screen w-full">
+      <Image
+        src={BgImage}
+        alt="bg-image"
+        className="z-0 w-full h-full object-cover absolute"
+      />
+      <div className="z-10 w-full flex flex-col items-center py-10 px-14">
+        <Image
+          src={ProfileImg}
+          alt="profile-img"
+          className="border-4 border-white z-10 rounded-full w-40 aspect-square"
+        />
+        <h1 className="mt-6 text-white font-semibold z-10 bg-zinc-900/40 backdrop-blur-md px-4 py-2.5 rounded-full border-2 border-zinc-800">
+          @Craftzmen
+        </h1>
+        <div className="max-w-md mx-auto flex flex-col gap-5 text-white mt-10 lg:mt-20 w-full">
+          <LinkItem
+            icon={<LinkedIn />}
+            url={"https://www.linkedin.com/in/abdullah-imran-592550267/"}
+          >
+            Linkedin
+          </LinkItem>
+          <LinkItem icon={<Github />} url={"https://github.com/Craftzmen/"}>
+            Github
+          </LinkItem>
+          <LinkItem
+            icon={<Medium />}
+            url={"https://medium.com/@abdullah.imran.code/"}
+          >
+            Medium
+          </LinkItem>
+          <LinkItem icon={<Twitter />} url={"https://x.com/Craftzmen/"}>
+            Twitter
+          </LinkItem>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
 export default Page;
