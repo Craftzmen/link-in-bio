1e2260a6d1930cbf8ced6d807bb8e5a7a14035e5
Abdullah Imran <abdullah.imran.code@gmail.com>
2024-04-28 10:57:43 +0500

    fix(meta): description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Craftzmen | Link in Bio page",
-  description: "Abdullah Imran, An Indie app developer currently living in pakistan persuing in tech, Has the ability to add creativity to anything. Loves to code anything #programmer",
+  description: "Abdullah Imran, an indie app developer currently living in Pakistan pursuing tech, has the ability to add creativity to anything. Loves to code anything #programmer",
   keywords: "portfolio, links, developer"
 };
 
